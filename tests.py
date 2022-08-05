f6cbe7ca58bc79f725c48cf2005b6e1d889f5415
Sushma Nagaraj <snsushma94@gmail.com>
2022-08-04 20:07:46 -0500

    testing here

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from hello import app
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data == b'Hello World!'
+    assert response.data == b'Hello World to me!'
     assert response.status_code == 200
